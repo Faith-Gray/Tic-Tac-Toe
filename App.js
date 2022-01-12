1e6a38cc1863e5ba3f4f755e8f4cc1120d0bcdf9
Faith <fhgray96@gmail.com>
2022-01-12 15:40:35 -0600

    added event listener and data type

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-squares = getElementsByClassName('square');
+const squares = document.querySelectorAll('[data-square]');
 
 const gameBoard = (() => {
     boardArray = new Array(9);
 })();
 
-console.log(boardArray.length);
+squares.forEach(square => {
+    square.addEventListener('click', squareClicked, {once: true})
+});
 
-squares.addEventListener('click', playRound());
+function squareClicked() {
+    console.log('clicked');
+    //mark x or o
+    //check for win or tie
+    //change turn
 
-const playRound
+};
 
 //const playRound = 
 //p1 pick
@@ -17,3 +23,5 @@ const playRound
 //have a variable for whose turn it is. 
 //after every click have it swap turns 
 // DUH. still not sure how to use factories though but fuck that
+//how do I get the cells of the grid to correspond with the array
+
