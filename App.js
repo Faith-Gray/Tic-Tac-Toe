2f5200c2867d909ac1fbf20d4e7bb082b1d82a2c
Faith <fhgray96@gmail.com>
2022-01-11 20:10:43 -0600

    started this project, updated html and css. JS needs work

---
+++ App.js
@@ -0,0 +1,19 @@
+squares = getElementsByClassName('square');
+
+const gameBoard = (() => {
+    boardArray = new Array(9);
+})();
+
+console.log(boardArray.length);
+
+squares.addEventListener('click', playRound());
+
+const playRound
+
+//const playRound = 
+//p1 pick
+//checkWin()
+//p2 pick checkWin()
+//have a variable for whose turn it is. 
+//after every click have it swap turns 
+// DUH. still not sure how to use factories though but fuck that
